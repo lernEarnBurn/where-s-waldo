8630b6a442ef3d8077eac50f41f34341d1eea178
lernEarnBurn <lerners124@gmail.com>
2023-12-06 14:48:44 +0200

    initialized express backend

---
+++ waldo-api/app.js
@@ -0,0 +1,25 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT , () => {
+  console.log(`Server is running on port ${PORT}`);
+})
+
+module.exports = app;
