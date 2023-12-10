7f432d76d47d6ef7bb6359fac345d2a781121610
lernEarnBurn <lerners124@gmail.com>
2023-12-10 14:27:16 +0200

    added some dummy data

--- waldo-api/populatePlayersDb.js
+++ waldo-api/populatePlayersDb.js
@@ -1,5 +1,12 @@
-const db = require('./dbConfig.js').playersDb
 const isPlayer = require('./models/playerSchema')
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./db/players.sqlite', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the database.');
+});
 
 // Create 'players' table
 db.run(`
@@ -13,10 +20,19 @@ db.run(`
 // Insert sample data
 const samplePlayers = [
   { id: '2134', name: 'Player1', runs: [{ level: 'Level1', totalSeconds: 30 }, { level: 'Level2', totalSeconds: 45 }] },
-  { id: '325325', name: 'Player2', runs: [{ level: 'Level1', totalSeconds: 25 }, { level: 'Level3', totalSeconds: 50 }] }
+  { id: '325325', name: 'Player2', runs: [{ level: 'Level1', totalSeconds: 25 }, { level: 'Level3', totalSeconds: 50 }] },
+  { id: '4785', name: 'Player3', runs: [{ level: 'Level1', totalSeconds: 40 }, { level: 'Level2', totalSeconds: 55 }] },
+  { id: '9071', name: 'Player4', runs: [{ level: 'Level1', totalSeconds: 28 }, { level: 'Level3', totalSeconds: 48 }] },
+  { id: '6542', name: 'Player5', runs: [{ level: 'Level2', totalSeconds: 35 }, { level: 'Level3', totalSeconds: 60 }] },
+  { id: '1239', name: 'Player6', runs: [{ level: 'Level1', totalSeconds: 32 }, { level: 'Level2', totalSeconds: 50 }] },
+  { id: '8765', name: 'Player7', runs: [{ level: 'Level2', totalSeconds: 42 }, { level: 'Level3', totalSeconds: 55 }] },
+  { id: '2346', name: 'Player8', runs: [{ level: 'Level1', totalSeconds: 38 }, { level: 'Level3', totalSeconds: 52 }] },
+  { id: '5432', name: 'Player9', runs: [{ level: 'Level1', totalSeconds: 33 }, { level: 'Level2', totalSeconds: 46 }] },
+  { id: '7890', name: 'Player10', runs: [{ level: 'Level2', totalSeconds: 37 }, { level: 'Level3', totalSeconds: 58 }] },
+  { id: '1357', name: 'Player11', runs: [{ level: 'Level1', totalSeconds: 29 }, { level: 'Level2', totalSeconds: 47 }] },
 ];
 
-const insertPlayer = db.prepare('INSERT INTO players (id, name, runs) VALUES (?, ?, ?)'); // <-- Update here
+const insertPlayer = db.prepare('INSERT INTO players (id, name, runs) VALUES (?, ?, ?)'); 
 samplePlayers.forEach(player => {
   insertPlayer.run(player.id, player.name, JSON.stringify(player.runs));
 });
