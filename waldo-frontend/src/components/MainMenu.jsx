ee9af7ecc71245488754068235657f19f98a5fa6
lernEarnBurn <lerners124@gmail.com>
2023-12-13 23:48:28 +0200

    added level cube

--- waldo-frontend/src/components/MainMenu.jsx
+++ waldo-frontend/src/components/MainMenu.jsx
@@ -5,8 +5,27 @@ export function MainMenu(){
     <motion.section 
      className="flex justify-center items-center h-[100vh] w-[100vw]"
      initial={{ y: 1000 }} animate={{ y: 0 }} exit={{ y: 1000 }} transition={{ duration: 0.3 }}
-    >
-      <p>Main Menu</p>
+    > 
+      <div className='flex flex-col gap-2'>
+        <div className='row flex gap-2'>
+          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+        </div>
+        <div className='row flex gap-2'>
+          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+        </div>
+        <div className='row flex gap-2'>
+          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+        </div>
+      </div>
     </motion.section>
   )
 }
