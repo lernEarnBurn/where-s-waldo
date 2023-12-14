5c825a4c0eee7c2c136880d7111b7979e0e89e74
lernEarnBurn <lerners124@gmail.com>
2023-12-14 13:43:30 +0200

    styled and added lock icon

--- waldo-frontend/src/components/MainMenu.jsx
+++ waldo-frontend/src/components/MainMenu.jsx
@@ -1,29 +1,30 @@
 import { motion } from 'framer-motion'
+import { LockKeyhole } from 'lucide-react';
 
 export function MainMenu(){
   return (
     <motion.section 
-     className="flex justify-center items-center h-[100vh] w-[100vw]"
+     className="bg-color flex justify-center items-center h-[100vh] w-[100vw]"
      initial={{ y: 1000 }} animate={{ y: 0 }} exit={{ y: 1000 }} transition={{ duration: 0.3 }}
     > 
-      <div className='flex flex-col gap-2'>
-        <div className='row flex gap-2'>
-          <div className='bg-red-400 w-24 h-24 level-cube'></div>
-          <div className='bg-red-400 w-24 h-24 level-cube'></div>
-          <div className='bg-red-400 w-24 h-24 level-cube'></div>
-          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+      <div className='flex flex-col gap-8'>
+        <div className='row flex gap-10'>
+          <div className='bg-red-400 w-36 h-36 level-cube'>1</div>
+          <div className='bg-red-400 w-36 h-36 level-cube'>2</div>
+          <div className='bg-red-400 w-36 h-36 level-cube'>3</div>
+          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
         </div>
-        <div className='row flex gap-2'>
-          <div className='bg-red-400 w-24 h-24 level-cube'></div>
-          <div className='bg-red-400 w-24 h-24 level-cube'></div>
-          <div className='bg-red-400 w-24 h-24 level-cube'></div>
-          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+        <div className='row flex gap-10'>
+          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
+          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
+          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
+          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
         </div>
-        <div className='row flex gap-2'>
-          <div className='bg-red-400 w-24 h-24 level-cube'></div>
-          <div className='bg-red-400 w-24 h-24 level-cube'></div>
-          <div className='bg-red-400 w-24 h-24 level-cube'></div>
-          <div className='bg-red-400 w-24 h-24 level-cube'></div>
+        <div className='row flex gap-10'>
+          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
+          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
+          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
+          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
         </div>
       </div>
     </motion.section>
