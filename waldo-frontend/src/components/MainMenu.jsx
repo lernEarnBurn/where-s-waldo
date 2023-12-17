00e49cc18e7382648dcff51a7fa018858d6dd514
lernEarnBurn <lerners124@gmail.com>
2023-12-17 15:12:40 +0200

    added lock shake animation for locked levels

--- waldo-frontend/src/components/MainMenu.jsx
+++ waldo-frontend/src/components/MainMenu.jsx
@@ -1,32 +1,115 @@
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 import { LockKeyhole } from 'lucide-react';
 
 export function MainMenu(){
+
+
   return (
     <motion.section 
-     className="bg-color flex justify-center items-center h-[100vh] w-[100vw]"
-     initial={{ y: 1000 }} animate={{ y: 0 }} exit={{ y: 1000 }} transition={{ duration: 0.3 }}
+      className="bg-color flex justify-center items-center h-[100vh] w-[100vw]"
+      initial={{ y: 1000 }} animate={{ y: 0 }} exit={{ y: 1000 }} transition={{ duration: 0.3 }}
     > 
       <div className='flex flex-col gap-8'>
         <div className='row flex gap-10'>
           <div className='bg-red-400 w-36 h-36 level-cube'>1</div>
           <div className='bg-red-400 w-36 h-36 level-cube'>2</div>
           <div className='bg-red-400 w-36 h-36 level-cube'>3</div>
-          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
+          <motion.div  
+            className='bg-red-400 w-36 h-36 level-cube'>
+              <motion.div
+                className='h-full grid place-items-center'
+                initial={{ rotate: 0 }}
+                whileTap={{ rotate: [0, -10, 10, -10, 10, 0], transition: { duration: 0.5 } }}
+               >
+                <LockKeyhole className='w-36 h-16'/>
+              </motion.div>
+            </motion.div>
         </div>
         <div className='row flex gap-10'>
-          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
-          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
-          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
-          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
-        </div>
-        <div className='row flex gap-10'>
-          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
-          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
-          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
-          <div className='bg-red-400 w-36 h-36 level-cube'><LockKeyhole className='w-36 h-16'/></div>
-        </div>
+          <motion.div  
+              className='bg-red-400 w-36 h-36 level-cube'>
+                <motion.div
+                  className='h-full grid place-items-center'
+                  initial={{ rotate: 0 }}
+                  whileTap={{ rotate: [0, -10, 10, -10, 10, 0], transition: { duration: 0.5 } }}
+                 >
+                  <LockKeyhole className='w-36 h-16'/>
+                </motion.div>
+              </motion.div>
+              <motion.div  
+              className='bg-red-400 w-36 h-36 level-cube'>
+                <motion.div
+                  className='h-full grid place-items-center'
+                  initial={{ rotate: 0 }}
+                  whileTap={{ rotate: [0, -10, 10, -10, 10, 0], transition: { duration: 0.5 } }}
+                 >
+                  <LockKeyhole className='w-36 h-16'/>
+                </motion.div>
+              </motion.div>
+              <motion.div  
+              className='bg-red-400 w-36 h-36 level-cube'>
+                <motion.div
+                  className='h-full grid place-items-center'
+                  initial={{ rotate: 0 }}
+                  whileTap={{ rotate: [0, -10, 10, -10, 10, 0], transition: { duration: 0.5 } }}
+                 >
+                  <LockKeyhole className='w-36 h-16'/>
+                </motion.div>
+              </motion.div>
+              <motion.div  
+              className='bg-red-400 w-36 h-36 level-cube'>
+                <motion.div
+                  className='h-full grid place-items-center'
+                  initial={{ rotate: 0 }}
+                  whileTap={{ rotate: [0, -10, 10, -10, 10, 0], transition: { duration: 0.5 } }}
+                 >
+                  <LockKeyhole className='w-36 h-16'/>
+                </motion.div>
+              </motion.div>
+          </div>
+          <div className='row flex gap-10'>
+          <motion.div  
+              className='bg-red-400 w-36 h-36 level-cube'>
+                <motion.div
+                  className='h-full grid place-items-center'
+                  initial={{ rotate: 0 }}
+                  whileTap={{ rotate: [0, -10, 10, -10, 10, 0], transition: { duration: 0.5 } }}
+                 >
+                  <LockKeyhole className='w-36 h-16'/>
+                </motion.div>
+              </motion.div>
+              <motion.div  
+              className='bg-red-400 w-36 h-36 level-cube'>
+                <motion.div
+                  className='h-full grid place-items-center'
+                  initial={{ rotate: 0 }}
+                  whileTap={{ rotate: [0, -10, 10, -10, 10, 0], transition: { duration: 0.5 } }}
+                 >
+                  <LockKeyhole className='w-36 h-16'/>
+                </motion.div>
+              </motion.div>
+              <motion.div  
+              className='bg-red-400 w-36 h-36 level-cube'>
+                <motion.div
+                  className='h-full grid place-items-center'
+                  initial={{ rotate: 0 }}
+                  whileTap={{ rotate: [0, -10, 10, -10, 10, 0], transition: { duration: 0.5 } }}
+                 >
+                  <LockKeyhole className='w-36 h-16'/>
+                </motion.div>
+              </motion.div>
+              <motion.div  
+              className='bg-red-400 w-36 h-36 level-cube'>
+                <motion.div
+                  className='h-full grid place-items-center'
+                  initial={{ rotate: 0 }}
+                  whileTap={{ rotate: [0, -10, 10, -10, 10, 0], transition: { duration: 0.5 } }}
+                 >
+                  <LockKeyhole className='w-36 h-16'/>
+                </motion.div>
+              </motion.div>
+          </div>
       </div>
     </motion.section>
-  )
+  );
 }
