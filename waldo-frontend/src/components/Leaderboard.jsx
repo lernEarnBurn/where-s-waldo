e3dc1bed23faa183a852eb5d644024b9411c6aab
lernEarnBurn <lerners124@gmail.com>
2023-12-31 15:29:59 +0200

    added getLeaderboard hook

--- waldo-frontend/src/components/Leaderboard.jsx
+++ waldo-frontend/src/components/Leaderboard.jsx
@@ -1,5 +1,35 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 export function Leaderboard(){
+
+  const { leaderboard } = useGetLeaderboards('Level1')
+
   return (
-    <>Leaderboard</>
+    <>
+    {leaderboard.map((item, index) => {
+      return <p key={index}>{item.name}</p>
+    })}
+    </>
   )
+}
+
+function useGetLeaderboards(level){
+  const [Leaderboard, setLeaderBoard] = useState([])
+
+  useEffect(() => {
+    const getLeaderboard = async() => {
+      try {
+        const response = await axios.get(`http://localhost:3000/level/${level}`)
+        setLeaderBoard(response.data)
+        console.log(response.data)
+      } catch (err){
+        console.log(err)
+      }
+    }
+
+    getLeaderboard()
+  }, [])
+
+  return { Leaderboard }
 }
