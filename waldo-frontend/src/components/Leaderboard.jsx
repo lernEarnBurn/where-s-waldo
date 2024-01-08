2cceee6e599c37b688fd057055e5216b355cedb2
lernEarnBurn <lerners124@gmail.com>
2024-01-08 15:41:23 +0200

    light styling to the leaderboard

--- waldo-frontend/src/components/Leaderboard.jsx
+++ waldo-frontend/src/components/Leaderboard.jsx
@@ -7,17 +7,24 @@ export function Leaderboard(){
   
 
   return (
-    <>
-   <>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        leaderboard.map((item, index) => (
-          <p className='text-red-500' key={index}>{item.name}</p>
-        ))
-      )}
-    </>
-    </>
+   <section className='flex gap-2 flex-col items-center justify-center'>
+    <div className='flex gap-56 mt-9'>
+      <h1 className='relative left-5 text-2xl header-clr'>Leaders</h1>
+      <h1 className='text-2xl header-clr'>Seconds</h1>
+    </div>
+    <div className=' rounded-lg border-red-400 border-2'>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          leaderboard.map((item, index) => (
+            <div key={index} className={`flex justify-between py-1 px-10 w-[30vw] ${index > 0 ? 'border-t-2 border-red-400' : ''}`}>
+              <p className='text-xl ml-3'>{item.name}</p>
+              <p className='text-xl mr-6'>{item.totalSeconds}</p>
+            </div>
+          ))
+        )}
+      </div>
+    </section>
   )
 }
 
